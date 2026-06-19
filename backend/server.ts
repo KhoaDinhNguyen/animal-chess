31ed5feebd68db62f4b61ce5317c97beed65f677
Ngoc Dinh Khoa Nguyen <khoacode1305@gmail.com>
2026-06-19 15:19:32 -0700

    fix: cors bug

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import http from "http";
 import SocketServer from "./socketServer";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 const server = http.createServer(app);
 SocketServer.registerSocketServer(server);
